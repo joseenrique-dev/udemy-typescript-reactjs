b71e2310b20b684cb83be3107fe0232f931b4195
José Enrique <otipip90@gmail.com>
2021-11-23 01:45:08 -0500

    working with ref and effectLayout ion customHooks

--- src/bases/CounterHook.tsx
+++ src/bases/CounterHook.tsx
@@ -5,12 +5,14 @@ interface Props {
 }
 
 export const CounterHook = ({ initialValue = 5 }: Props) => {
-  const { counter, counterElement, handleClick } = useCounter();
+  const { counter, elementToAnimate, handleClick } = useCounter({
+    maxCount: 15,
+  });
 
   return (
     <>
       <h1>Counter Hook: {counter}</h1>
-      <h2 ref={counterElement}>{counter}</h2>
+      <h2 ref={elementToAnimate}>{counter}</h2>
       <button onClick={handleClick}>+1</button>
     </>
   );
--- src/hooks/useCounter.ts
+++ src/hooks/useCounter.ts
@@ -1,10 +1,12 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useLayoutEffect, useRef, useState } from 'react';
 import { gsap } from 'gsap';
 
 const MAXIMUN_COUNT = 10;
-export const useCounter = () => {
+
+export const useCounter = ({ maxCount = 1 }) => {
   const [counter, setCounter] = useState(5);
-  const counterElement = useRef<HTMLHeadingElement>(null);
+  const elementToAnimate = useRef<any>(null);
+  const tl = useRef(gsap.timeline());
 
   const handleClick = () => {
     // setCounter((prev) => {
@@ -13,21 +15,25 @@ export const useCounter = () => {
     setCounter((prev) => Math.min(prev + 1, MAXIMUN_COUNT));
   };
 
-  useEffect(() => {
-    if (counter < 10) return;
-    console.log('%cMax number is here', 'color:red');
+  useLayoutEffect(() => {
+    if (!elementToAnimate.current) return;
+    tl.current
+      .to(elementToAnimate.current, {
+        y: -10,
+        duration: 0.2,
+        ease: 'ease.out',
+      })
+      .to(elementToAnimate.current, { y: 0, duration: 1, ease: 'bounce.out' })
+      .pause();
+  }, []);
 
-    const tl = gsap.timeline();
-    tl.to(counterElement.current, {
-      y: -10,
-      duration: 0.2,
-      ease: 'ease.out',
-    }).to(counterElement.current, { y: 0, duration: 1, ease: 'bounce.out' });
+  useEffect(() => {
+    tl.current.play(0);
   }, [counter]);
 
   return {
     counter,
-    counterElement,
+    elementToAnimate,
     handleClick,
   };
 };
