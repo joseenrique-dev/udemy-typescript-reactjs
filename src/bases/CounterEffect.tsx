6916c9c54550e7588560d3713e28158099e053ed
José Enrique <otipip90@gmail.com>
2021-11-23 01:05:51 -0500

    working animations with gsap

--- src/bases/CounterEffect.tsx
+++ src/bases/CounterEffect.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-
+import { gsap } from 'gsap';
 interface Props {
   initialValue?: number;
 }
@@ -13,11 +13,19 @@ export const CounterEffect = ({ initialValue = 5 }: Props) => {
     setCounter((prev) => Math.min(prev + 1, MAXIMUN_COUNT));
   };
 
-  useEffect(() => {}, [counter]);
+  useEffect(() => {
+    if (counter < 10) return;
+    console.log('%cMax number is here', 'color:red');
+
+    gsap.to('h2', { y: -10, duration: 0.2, ease: 'ease.out' }).then(() => {
+      gsap.to('h2', { y: 0, duration: 1, ease: 'bounce.out' });
+    });
+  }, [counter]);
 
   return (
     <>
       <h1>CounterUseEffect: {counter}</h1>
+      <h2>{counter}</h2>
       <button onClick={handleClick}>+1</button>
     </>
   );
