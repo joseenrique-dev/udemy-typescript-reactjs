6ef84a5631fdeffd85d38aaed75055399a81eb44
José Enrique <otipip90@gmail.com>
2021-11-23 00:19:05 -0500

    handle object like state

---
+++ src/bases/Counter.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react';
+
+interface Props {
+  initialValue?: number;
+}
+
+export const Counter = ({ initialValue = 15 }: Props) => {
+  const [counter, setCounter] = useState(initialValue);
+  const handleClick = () => {
+    setCounter((prev) => prev + 1);
+  };
+
+  return (
+    <>
+      <h1>Counter: {counter}</h1>
+      <button onClick={handleClick}>+1</button>
+    </>
+  );
+};
---
+++ src/bases/CounterBy.tsx
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+
+interface Props {
+  initialValue?: number;
+}
+
+export const CounterBy = ({ initialValue = 5 }: Props) => {
+  const [counterState, setCounterState] = useState({
+    counter: initialValue,
+    clicks: 0,
+  });
+  const { counter, clicks } = counterState;
+  const handleCount = (value: number) => {
+    setCounterState(({ counter, clicks }) => ({
+      counter: counter + value,
+      clicks: clicks + 1,
+    }));
+  };
+
+  return (
+    <>
+      <h1>CounterBy: {counter}</h1>
+      <h1>Clicks: {clicks}</h1>
+      <button onClick={() => handleCount(1)}>+1</button>
+      <button onClick={() => handleCount(5)}>+5</button>
+    </>
+  );
+};
